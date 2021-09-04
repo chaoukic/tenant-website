c79d91a8125f23e7adeb8ac946fc1900f142ca1c
chaoukis <chaoukisammy@gmail.com>
2021-09-03 23:55:07 -0400

    Update theme

---
+++ src/components/authentication/register/RegisterForm.js
@@ -0,0 +1,132 @@
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { useNavigate } from 'react-router-dom';
+// material
+import { Stack, TextField, IconButton, InputAdornment } from '@material-ui/core';
+import { LoadingButton } from '@material-ui/lab';
+import axios from 'axios'
+
+// ----------------------------------------------------------------------
+
+export default function RegisterForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+  const RegisterSchema = Yup.object().shape({
+    firstName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('First name required'),
+    lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Last name required'),
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    phonenumber: Yup.string().matches(phoneRegExp,'Phone number is invalid').required('A phone number is required'),
+    password: Yup.string().required('Password is required'),
+
+  });
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      phonenumber:''
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: (values) => {
+      axios.post(
+        '/api/tenants/register',{
+          firstname:values.firstName,
+          lastname:values.lastName,
+          phonenumber:values.phonenumber,
+          email:values.email,
+          password: values.password
+        }
+      ).then((response) =>{
+        if (response.data.status){
+          navigate('/dashboard', { replace: true });
+        }
+      }).catch((error)=>{
+        console.log(error)
+      })
+      
+    }
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+            <TextField
+              fullWidth
+              label="First name"
+              {...getFieldProps('firstName')}
+              error={Boolean(touched.firstName && errors.firstName)}
+              helperText={touched.firstName && errors.firstName}
+            />
+
+            <TextField
+              fullWidth
+              label="Last name"
+              {...getFieldProps('lastName')}
+              error={Boolean(touched.lastName && errors.lastName)}
+              helperText={touched.lastName && errors.lastName}
+            />
+          </Stack>
+
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+          <TextField
+            fullWidth
+            autoComplete="phonenumber"
+            type="text"
+            label="Phone number"
+            {...getFieldProps('phonenumber')}
+            error={Boolean(touched.phonenumber && errors.phonenumber)}
+            helperText={touched.phonenumber && errors.phonenumber}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Register
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
+  );
+}
+
